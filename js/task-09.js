51018fbb38d97dcab05d7236bf48423d7ee79b8d
Andrii Veremii <dashuk10@gmail.com>
2023-02-13 18:32:07 +0200

    add

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// };
+
+// const colorEl = document.querySelector('.color');
+// const btnChangeColorEl = document.querySelector('.change-color');
+// const bodyEl = document.body;
+
+// const getColorBody = () => {
+//   colorEl.textContent = getRandomHexColor()
+//   bodyEl.style.backgroundColor = getRandomHexColor()
+// };
+
+// btnChangeColorEl.addEventListener('click', getColorBody);
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
@@ -7,8 +25,9 @@ const btnChangeColorEl = document.querySelector('.change-color');
 const bodyEl = document.body;
 
 const getColorBody = () => {
-  colorEl.textContent = getRandomHexColor()
-  bodyEl.style.backgroundColor = getRandomHexColor()
+  let randomHexColor = getRandomHexColor()
+  colorEl.textContent = randomHexColor
+  bodyEl.style.backgroundColor = randomHexColor
 };
 
 btnChangeColorEl.addEventListener('click', getColorBody);
